45e63443a9f0677eef19d168dbe427546a87aac4
Bogdan Martinescu <bogdan.martinescu@hartmannindustries.com>
2020-09-22 02:27:39 +0300

    initial setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyCgyCHXoeD9N33djToTjN1xVJDNyavI6ak",
+    authDomain: "clone-978e4.firebaseapp.com",
+    databaseURL: "https://clone-978e4.firebaseio.com",
+    projectId: "clone-978e4",
+    storageBucket: "clone-978e4.appspot.com",
+    messagingSenderId: "111661958598",
+    appId: "1:111661958598:web:7860ea1f17f8d3023cdec7",
+    measurementId: "G-PBCNFEN00P"
+};
+
+export default config;
